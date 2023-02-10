591f8a271dd6dcf97a74f873077f833a0221bdb5
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-10 17:56:54 +0200

    add bootstrap server logic

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
-import {Controller, Get, Module} from '@nestjs/common';
-
+import { Controller, Get, Module } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
 
 @Controller()
 class AppController {
-    @Get()
-    getRootRoute() {
-        return 'Hi there';
-    }
+  @Get()
+  getRootRoute() {
+    return "Hi there";
+  }
 }
+
+@Module({
+  controllers: [AppController],
+})
+class AppModule {}
+
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(3000);
+};
+
+bootstrap();
