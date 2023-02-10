f40d823a013722725c5aed484f2d576a52ecb2e2
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-10 18:04:27 +0200

    split logic into files

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,6 @@
 import { Controller, Get, Module } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-
-@Controller()
-class AppController {
-  @Get()
-  getRootRoute() {
-    return "Hi there";
-  }
-}
-
-@Module({
-  controllers: [AppController],
-})
-class AppModule {}
+import { AppModule } from "./app.module";
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
