0b3e4d7229461f634e17872a589be5f32d98ae39
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-10 19:04:16 +0200

    add base routes

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  getOne() {
+    return 'all';
+  }
+
+  @Get(':id')
+  getOneById(@Param('id') messagesId: number) {
+    return messagesId;
+  }
+
+  @Post()
+  createOne() {
+    return 'created';
+  }
+}
