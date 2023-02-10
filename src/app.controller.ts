cee4f623c1fd029db2bf1ee9afab1ad2af68d807
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-10 18:47:18 +0200

    regenerate project with nest cli

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Get()
-  getRootRoute() {
-    return "Hi there";
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
-const bootstrap = async () => {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   await app.listen(3000);
-};
-
+}
 bootstrap();
